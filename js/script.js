0272623f7e984d6672ba124707b5a66c0a95be23
Saul Chavez <saulchavez307@gmail.com>
2025-07-02 17:43:03 -0700

    quote fade functionality added with javascript
    Resources used: https://www.geeksforgeeks.org/javascript/javascript-anonymous-functions/
    
    https://developer.mozilla.org/en-US/docs/Glossary/Callback_function
    
    I learned arrow functions in js through this process

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-const quotes = [
-    `"The world is flat if you believe it is flat." – Albert Einstein`,
-    `"If you are first you are not last." – Unknown`,
-    `"Education is real" – Malcolm X`,
-    `":)" – GenOne Compass`
-  ];
+document.addEventListener("DOMContentLoaded", () => {
+    const quotes = [
+        `"The world is flat if you believe it is flat." – Albert Einstein`,
+        `"If you are first you are not last." – Unknown`,
+        `"Education is real" – Malcolm X`,
+        `":)" – GenOne Compass`
+    ];
 
-const targetElement = document.getElementById("dynamic-quote");  
+    const targetElement = document.getElementById("dynamic-quote");  
 
-let curr = 0;
+    let curr = 0;
 
-function changeText() {
-    targetElement.innerText = quotes[curr];
-    curr = (curr + 1) % quotes.length; 
-}
+    function changeText() {
+        targetElement.classList.add("hero-quote-fade-out"); // Has opacity start at zero
 
-setInterval(changeText, 3000); 
+        setTimeout(() => {
+            curr = (curr + 1) % quotes.length; // Allows advancement while returning to 0
+            targetElement.innerText = quotes[curr];
+            targetElement.classList.remove("hero-quote-fade-out"); // Ensures opacity increases over 0.8s
+        }, 800); // 800 ms lines up with increase in opacity over 0.8s
+    }
+
+    setInterval(changeText, 5000); // Pauses quote for 5 seconds
+});
